0f142df25cb4325594a6079f666067167d2db300
Gary Bleads <gary@bleads.co.uk>
2025-07-18 14:53:17 +0100

    Changed IP Address for copier 2

--- setup_ap.sh
+++ setup_ap.sh
@@ -4,11 +4,11 @@
 nmcli con delete Copier_AP 2>/dev/null
 
 # Create AP configuration
-nmcli con add type wifi ifname wlan0 mode ap con-name Copier_AP ssid Copier_AP autoconnect yes
+nmcli con add type wifi ifname wlan0 mode ap con-name Copier_AP ssid Copier2_AP autoconnect yes
 nmcli con modify Copier_AP 802-11-wireless.band bg
 nmcli con modify Copier_AP 802-11-wireless.channel 11
 nmcli con modify Copier_AP wifi-sec.key-mgmt wpa-psk
 nmcli con modify Copier_AP wifi-sec.psk "LetMeIn123"
-nmcli con modify Copier_AP ipv4.method shared ipv4.address 192.168.4.1/24
+nmcli con modify Copier_AP ipv4.method shared ipv4.address 192.168.4.2/24
 nmcli con modify Copier_AP ipv6.method disabled
 nmcli con up Copier_AP
